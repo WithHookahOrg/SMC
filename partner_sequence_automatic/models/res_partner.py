81df3ee145db098273e2a98b10f791111d5dc2ed
smcviltco <75363152+smcviltco@users.noreply.github.com>
2021-05-10 16:34:55 +0500

    Add files via upload

--- partner_sequence_automatic/models/res_partner.py
+++ partner_sequence_automatic/models/res_partner.py
@@ -8,7 +8,10 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
     _description = "Res partner Sequences"
 
+    ref = fields.Char('Reference', required=True, copy=False,
+                                index=True, default=lambda self: _('/'))
+
     @api.model
     def create(self, vals):
-        vals['ref'] = self.env['ir.sequence'].next_by_code('res.partner') or '/'
+        vals['ref'] = self.env['ir.sequence'].next_by_code('res.partner.ref') or '/'
         return super(ResPartner, self).create(vals)
